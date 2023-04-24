9a63077647eda86340419ad8f89685ff270fbaf7
othaviogc <130675220+othaviogc@users.noreply.github.com>
2023-04-24 14:03:40 -0300

    Aprendendo Column

---
+++ main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(ComponenteInicial());
+}
+
+class ComponenteInicial extends StatelessWidget{
+
+  void eventoBotao(){
+    print("Clicou");
+  }
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(
+          title: Text("Perguntas e Respostas"),
+
+        ), 
+        body: Column(
+          children: [
+           ElevatedButton(onPressed: eventoBotao
+           child: Text("Cancelar"))
+ElevatedButton(onPressed:eventoBotao,
+           child: Text("Salvar")),
+
+     Column(
+children:
+Text ("Aprendendo"),
+Text( "Programação"),
+Text("Flutter"),
+
+          ]
+       ), 
+      ),
+    );
+  }
+}
