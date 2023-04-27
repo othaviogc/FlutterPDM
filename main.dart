9555b4f213c50a483e1e4b72f7959eb260fce726
othaviogc <130675220+othaviogc@users.noreply.github.com>
2023-04-27 11:32:10 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,41 +1,58 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main(){
-  runApp(ComponenteInicial());
+  runApp(AulaGrupoDois());
 }
 
-class ComponenteInicial extends StatelessWidget{
+class AulaGrupoDois extends StatefulWidget{
 
-  void eventoBotao(){
-    print("Clicou");
-  }
+  @override
 
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        appBar:AppBar(
-          title: Text("Perguntas e Respostas"),
-        ), 
-        body: Column(
-          children: [
-            ElevatedButton(
-              onPressed: eventoBotao,
-              child: Text("Cancelar"),
-            ),
-            ElevatedButton(
-              onPressed:eventoBotao,
-              child: Text("Salvar"),
-            ),
-            Column(
-              children: [
-                Text("Aprendendo"),
-                Text("Programação"),
-                Text("Flutter"),
-              ],
-            ),
-          ],
-        ), 
+  State<AulaGrupoDois> createState() => _AulaGrupoDoisState();
+}
+
+class _AulaGrupoDoisState extends State<AulaGrupoDois>{
+   var contador= 0;
+
+   final perguntas = [
+    "Qual a cor favorita?",
+    "Comida favorita?",
+    "Animal que mais gosta?",
+    "O que faz nas horas vagas?"
+    ];
+   
+  void clicou(){
+    setState(() {
+      contador: contador++;
+    });
+    print (contador);
+    }
+
+Widget build(BuildContext context){
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Perguntas e respostas"),
       ),
-    );
-  }
+      body: Column(
+        children: [
+          Text(perguntas[contador]),
+          ElevatedButton(
+            onPressed: eventoBotao,
+           child: Text("Enviar")
+           ),
+           ElevatedButton(
+            onPressed: (){
+              print ("Outra Função");
+            },
+           child: Text("Cancelar")),
+            ElevatedButton(
+              onPressed: () => print ("Função Arrow"),
+           child: Text("Salvar")),
+        ],
+       )   
+    )
+  );
+}
 }
