12b06cb7a98858b07731c5d9361b2fd32090b81b
othaviogc <130675220+othaviogc@users.noreply.github.com>
2023-05-11 10:56:58 -0300

    Add files via upload

---
+++ botao.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Botao extends StatelessWidget{
+
+  String texto;
+
+  Botao(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+  return Container(
+    width: double.infinity,
+    margin: EdgeInsets.all(30),
+     child: ElevatedButton(
+      onPressed:() {},
+       child: Text(texto),
+       style: ButtonStyle(
+        minimumSize: MaterialStateProperty.all(Size(50,50))
+    ),
+   );
+  )
+ }
+}
--- main.dart
+++ main.dart
@@ -1,58 +1,74 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './perguntas.dart';
 
-main(){
+main() {
   runApp(AulaGrupoDois());
 }
 
-class AulaGrupoDois extends StatefulWidget{
-
+class AulaGrupoDois extends StatefulWidget {
   @override
-
   State<AulaGrupoDois> createState() => _AulaGrupoDoisState();
 }
 
-class _AulaGrupoDoisState extends State<AulaGrupoDois>{
-   var contador= 0;
+class _AulaGrupoDoisState extends State<AulaGrupoDois> {
+  var contador = 0;
 
-   final perguntas = [
+  final perguntas = [
     "Qual a cor favorita?",
     "Comida favorita?",
     "Animal que mais gosta?",
     "O que faz nas horas vagas?"
-    ];
-   
-  void clicou(){
-    setState(() {
-      contador: contador++;
-    });
-    print (contador);
-    }
+  ];
 
-Widget build(BuildContext context){
-  return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Perguntas e respostas"),
+  @override
+  void clicou() {
+    setState(() {
+      contador = contador + 1;
+    });
+    print(contador);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Jogo das perguntas"),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[contador]),
+            Perguntas(perguntas[contador]),
+            ElevatedButton(onPressed: clicou, child: Text("Azul")),
+            ElevatedButton(onPressed: null, child: Text("Verde")),
+            ElevatedButton(onPressed: null, child: Text("Vermelho")),
+            ElevatedButton(onPressed: null, child: Text("Amarelo")),
+            NovoBotao(
+              texto: "Novo Botão",
+              acao: () {
+                print("Clicou no Novo Botão!");
+              },
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Text(perguntas[contador]),
-          ElevatedButton(
-            onPressed: eventoBotao,
-           child: Text("Enviar")
-           ),
-           ElevatedButton(
-            onPressed: (){
-              print ("Outra Função");
-            },
-           child: Text("Cancelar")),
-            ElevatedButton(
-              onPressed: () => print ("Função Arrow"),
-           child: Text("Salvar")),
-        ],
-       )   
-    )
-  );
+    );
+  }
 }
+
+class NovoBotao extends StatelessWidget {
+  final String texto;
+  final VoidCallback acao;
+
+  const NovoBotao({Key? key, required this.texto, required this.acao})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: acao,
+      child: Text(texto),
+    );
+  }
 }
